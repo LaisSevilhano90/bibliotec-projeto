fc164580e2bbfac3faae5bd4878b2a6cc5a229bb
LAIS RODRIGUES SEVILHANO <56132300805@info.local>
2026-09-04 17:27:23 -0300

    tela de login adicionada

---
+++ login/login.js
@@ -0,0 +1,7 @@
+const campoSenha = document.getElementById("senha");
+const btnSenha = document.querySelector("#mostrar-senha");
+console.log(btnSenha);
+
+btnSenha.addEventListener("click", function(){
+    alert("cliquei no botão!");
+})
